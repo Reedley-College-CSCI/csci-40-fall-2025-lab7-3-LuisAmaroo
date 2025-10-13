826e9dd016cc646e227e8fc3e1d46f7a77dc8871
Luis Amaro <luisamlo559@gmail.com>
2025-10-13 13:35:58 -0700

    called readTemperatures to read data from file

--- array_of_structs.cpp
+++ array_of_structs.cpp
@@ -15,7 +15,7 @@ struct TemperatureRecord
 const int MAX_DAYS = 31;
 
 // Function Prototypes
-void readTemperatures(???); // TODO: Fix the parameters
+void readTemperatures(days, temps, sizeMax); // TODO: Fix the parameters
 void printTemperatures(const ???);
 TemperatureRecord findMin(const ???);
 TemperatureRecord findMax(const ???);
@@ -23,11 +23,12 @@ double findAverage(const ???);
 
 int main() {
     // TODO: Step 2 - Declare an array of TemperatureRecord structs (MAX_DAYS size)
-    TemperatureRecord day[MAX_DAYS], temperature[MAX_DAYS];
+    TemperatureRecord day[MAX_DAYS];
+    TemperatureRecord temperature[MAX_DAYS];
     int size = 0;  // Actual number of records read
-
-    // TODO: Step 3 - Call readTemperatures() to load data from file
     
+    // TODO: Step 3 - Call readTemperatures() to load data from file
+    readTemperatures(day, temperature, MAX_DAYS);
     // TODO: Step 4 - Print the temperatures
 
     // TODO: Step 5 - Compute and display min, max, and average temperature
